fc669829665a054258f4bfd91d0687ffebab0b55
DraconDev <dracsharp@gmail.com>
2023-10-13 11:03:09 +0100

    add less

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ def test2():
     print("test2")
 
 
-def test3():
-    print("test3")
-
-
-def test4():
-    print("test4")
-
-
-def test5():
-    print("test5")
-
-
 if __name__ == "__main__":
     test()
     test2()
